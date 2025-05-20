789559de4db2c1f6f5100c19882382f2f3c0facb
iftakhersifat <hossensifat615@gmail.com>
2025-05-20 21:18:09 +0600

    added post garden tip

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 // gardening-community
 // VzEpeoqZyG9jFbCJ
 
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mojyanw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,12 +27,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const tipsCollection = client.db("gardeningDB").collection("tips");
+    // const userCollection = client.db("coffeeDB").collection("users");
+
+    // POST garden tip
+    app.post('/garden-tips', async (req, res) => {
+    const newTips = req.body;
+    console.log('Garden-Tips:', newTips);
+    const result = await tipsCollection.insertOne(newTips)
+    res.send(result)
+    });
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
